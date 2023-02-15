c6d667af13166a909ec8c54ad790c6a8e0b78e8c
yeooong-dev <yeooong12@naver.com>
2023-02-15 11:36:12 +0900

    finish

--- src/components/Users.js
+++ src/components/Users.js
@@ -10,7 +10,12 @@ const User = ({item})=> {
     return(
       <div key = {item.id} className='content'>
         <div className='buttonbox'>
-          <Link to={`/todos/${item.id}`}>상세보기</Link>
+          <Link style={{
+            border:"none",
+            fontSize:"1.1rem",
+            marginBottom:"10px",
+            padding:"3px"
+            }} to={`/todos/${item.id}`}>상세보기</Link>
         {item.title}-{item.memo}
           <button className='delete_btn' onClick={()=>dispatch(removeTodos(item))}>삭제</button>
           {item.isDone === false ? <button className='clear_btn' onClick={()=>dispatch(toggleTodos(item))}>완료</button> : <button className='clear_btn' onClick={()=>dispatch(toggleTodos(item))}>취소</button>}
--- src/pages/Todos.js
+++ src/pages/Todos.js
@@ -3,6 +3,70 @@ import { useSelector } from 'react-redux'
 import { useNavigate, useParams } from 'react-router-dom'
 import styled from 'styled-components'
 
+// 위에 고정
+
+const StMain = styled.div`
+    position:relative;
+    width:1200px;
+    height:1080px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    background-color:#f6f6f6;
+    margin-top:50px;
+    border-radius:20px;
+    box-shadow: 6px 7px 5px 0px rgba(0,0,0,0.22);
+    -webkit-box-shadow: 6px 7px 5px 0px rgba(0,0,0,0.22);
+    -moz-box-shadow: 6px 7px 5px 0px rgba(0,0,0,0.22);
+`
+
+const StDiv = styled.div`
+    position:absolute;
+    top:500px;
+    font-size:2rem;
+    
+`
+const StBtn = styled.button`
+    width:160px; height:50px;
+    background-color:rgb(44, 43, 52);
+    border-radius:30px;
+    font-size:1.2rem;
+    font-weight:bold;
+    border:none;
+    color:white;
+    cursor:pointer;
+`
+
+const StBox = styled.div`
+    position:absolute;
+    top:60px;
+    width:80%;
+    padding:30px;
+    box-sizing: border-box;
+    align-items: center;
+    display:flex;
+    justify-content:space-between;
+    border-bottom:1px solid rgb(44, 43, 52);
+    margin-bottom:600px;
+`
+
+const StId = styled.div`
+    width:50px; height:50px;
+    text-align:center;
+    line-height:53px;
+    font-size:2rem;
+    background-color:rgb(44, 43, 52);
+    border-radius:50%;
+    color:white;
+`
+
+const StTitle = styled.div`
+    font-size:${(props) => props.fontSize}
+`
+
+
+
 function Todos() {
     const data = useSelector(state=>state.todos)
     const params = useParams()
@@ -10,19 +74,31 @@ function Todos() {
     const navigate = useNavigate()
 
   return (
-    <div>
+    <StMain>
+        <StBox>
+          <StId>
+             {findData.id}
+          </StId>
+
+          <StBtn onClick={()=>navigate(-1)}>뒤로가기</StBtn>
+        </StBox>  
+
         <StDiv>
-        {findData.id}
-        {findData.title}
-        {findData.memo}
+          <StTitle fontSize="3rem;">
+            {findData.title}
+          </StTitle>
+
+          <StTitle fontSize="2rem;">
+             {findData.memo}
+          </StTitle>
         </StDiv>
-      <button onClick={()=>navigate(-1)}>뒤로가기</button>
-    </div>
+
+          
+    </StMain>
+    
+    
   )
 }
 
 export default Todos
 
-const StDiv = styled.div`
-    font-size:1.5rem;
-`
